fb695ef8ec144a4c10e52d075193d97de9b6896c
deepudeepti <229x1a0596@gprec.ac.in>
2026-02-05 08:39:28 +0530

    Update Class1.java

--- src/pograms/Class1.java
+++ src/pograms/Class1.java
@@ -4,9 +4,14 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Deepthi");
+		System.out.println("Hello Deepthi");
+		System.out.println("Hello Deepthi");
+		System.out.println("Hello Deepthi");
+		System.out.println("Hello Deepthi");
 		// TODO Auto-generated method stub
 
 	}
 
 }
+
 
